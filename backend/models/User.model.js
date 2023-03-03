2124257c741b13223d23f52dc10d8063ac054b2a
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-03-03 10:35:33 +0530

    backend

---
+++ backend/models/User.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name:{type:String,required:true},
+    username:{type:String,required:true,unique:true},
+    phone:{type:Number,required:true,min:10,unique:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true,min:6,max:10},
+    gender:{type:String,required:true}
+  },
+  { versionKey: false }
+);
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = { UserModel };
