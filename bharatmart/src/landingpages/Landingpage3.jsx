d4bb0cd13805677651978ce6ce23ae544cb9b1ba
sachin4147 <sachinshivabugadi1998@gmail.com>
2023-03-03 10:11:02 +0530

    home

---
+++ bharatmart/src/landingpages/Landingpage3.jsx
@@ -0,0 +1,184 @@
+import { SimpleGrid,Box } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter,Button,Heading } from '@chakra-ui/react'
+import { Flex, Spacer } from '@chakra-ui/react'
+
+
+
+
+
+
+
+export  const LandingPage3=()=>{
+
+
+
+
+
+
+
+
+return <div>
+
+
+
+<Card width="98%"margin="auto" border="0px solid red"height="550px">
+
+
+    <Card fontSize="20px" textAlign="left"borderTop="3px solid rgb(46,49,146)"marginBottom="-20px"paddingTop="-40px">
+        <Heading>Pharmaceutical Drug, Medicine, Medical Care & Consultation</Heading>
+    </Card>
+   
+   <Flex >
+     <SimpleGrid border="0px solid yellow"   columns={[1]} width={[
+      "100%", 
+      "50%", 
+      "25%", 
+      "15%", 
+    ]} marginRight='20px'>
+     <Box  height="550px" width={[200,300,400]}px  backgroundImage="url('https://www.pixelstalk.net/wp-content/uploads/images1/Medicine-Wallpapers-HD.jpg')">
+     <Card
+     backgroundColor="none" 
+     boxShadow="none"
+marginTop="350px"
+color="white"
+width="250px"
+fontSize="17px"
+textAlign="left"
+lineHeight="30px"
+marginLeft="30px"
+>
+     <p>Pharma Raw Material
+Pharmaceutical Chemical
+Clinical Trial Services
+X-Ray Services</p></Card>
+<Button
+
+    width="120px"
+    height="40px"
+    borderRadius="5px"
+    border="none"
+    marginLeft="110px"
+    backgroundColor="red"
+
+>View All</Button>
+
+
+</Box>
+</SimpleGrid>
+
+
+<SimpleGrid columns={[1, 1, 1,3]} spacing='10px' marginLeft="200px">
+<Box  textAlign="left" display="flex" height='160px' width="300px" boxShadow=" rgba(99, 99, 99, 0.2) 0px 2px 8px 0px" border="0px solid red">
+      <Card marginTop="28px" marginLeft="15px"><img  height="110px"
+   src="https://5.imimg.com/data5/KT/MJ/MY-16675294/pharmaceutical-drug-125x125.jpg" alt="" /></Card>
+      <Card marginLeft="15px" boxShadow="  0px" marginTop="20px">
+      <h5 >Pharmaceutical drugs</h5>
+        <Card lineHeight="20px" border="0px solid red"fontSize="14px" boxShadow="  0px">
+        <p  >Anticipated drugs</p>
+        <p >Antiboitic tablets</p>
+        <p >Sryrups</p>
+        </Card>
+      </Card>
+     </Box>
+  <Box textAlign="left" display="flex" height='160px' width="300px" boxShadow=" rgba(99, 99, 99, 0.2) 0px 2px 8px 0px" border="0 solid red">
+      <Card marginTop="28px" marginLeft="15px"><img height="110px" src="https://3.imimg.com/data3/JR/DH/GLADMIN-7427/nutritional-supplements-125x125.jpg" alt="" /></Card>
+      <Card marginLeft="15px" boxShadow="  0px" marginTop="20px">
+      <h5 >Nutraceutics</h5>
+        <Card lineHeight="20px" border="0px solid red"fontSize="14px" boxShadow="  0px">
+        <p >Vitiman Tablets</p>
+        <p>Proximity Tablets</p>
+        <p>Vitiman Tablets</p>
+        
+        </Card>
+      </Card>
+     </Box>
+  <Box textAlign="left" display="flex" height='160px' width="300px" boxShadow=" rgba(99, 99, 99, 0.2) 0px 2px 8px 0px" border="0 solid red">
+      <Card marginTop="28px" marginLeft="15px"><img height="110px" src="https://5.imimg.com/data5/PQ/HW/MY-7191707/thinq-mass-gainer-282-lbs-29-125x125.png" alt="" /></Card>
+      <Card marginLeft="15px" boxShadow="  0px" marginTop="20px">
+      <h5 >Fiteness supliments</h5>
+        <Card lineHeight="20px" border="0px solid red"fontSize="14px" boxShadow="  0px">
+        <p  >weight gain</p>
+        <p >Motor gain</p>
+        <p >T weight gain</p>
+        </Card>
+      </Card>
+     </Box>
+  <Box textAlign="left" display="flex" height='160px' width="300px" boxShadow=" rgba(99, 99, 99, 0.2) 0px 2px 8px 0px" border="0 solid red">
+      <Card marginTop="28px" marginLeft="15px"><img height="110px" src="https://5.imimg.com/data5/CM/CP/JH/SELLER-5038493/neonatal-portable-icu-ventilator-125x125.jpg" alt="" /></Card>
+      <Card marginLeft="15px" boxShadow="  0px">
+      <h5 >Madical Treatments</h5>
+        <Card lineHeight="20px" border="0px solid red"fontSize="14px" boxShadow="  0px">
+        <p  >Dental</p>
+        <p >Surgery</p>
+        <p >EYE </p>
+        </Card>
+      </Card>
+     </Box>
+  <Box textAlign="left" display="flex" height='160px' width="300px" boxShadow=" rgba(99, 99, 99, 0.2) 0px 2px 8px 0px" border="0 solid red">
+      <Card marginTop="28px" marginLeft="15px"><img height="110px" src="https://5.imimg.com/data5/OQ/SV/MY-905156/stethoscope-cardio-125x125.jpg" alt="" /></Card>
+      <Card marginLeft="15px" boxShadow="  0px">
+      <h5 >Medical Test survices</h5>
+        <Card lineHeight="20px" border="0px solid red"fontSize="14px" boxShadow="  0px">
+        <p  >CT Scan</p>
+        <p >Ultra Sound</p>
+        <p >ECHO</p>
+        
+        </Card>
+      </Card>
+     </Box>
+     <Box textAlign="left" display="flex" height='160px' width="300px" boxShadow=" rgba(99, 99, 99, 0.2) 0px 2px 8px 0px" border="0 solid red">
+      <Card marginTop="28px" marginLeft="15px"><img height="110px" src="https://5.imimg.com/data5/YC/QB/MY-3773531/allopathic-pcd-pharma-franchise-125x125.jpg" alt="" /></Card>
+      <Card marginLeft="15px" boxShadow="  0px">
+      <h5 >PCD pharma </h5>
+        <Card lineHeight="20px" border="0px solid red"fontSize="14px"  boxShadow="  0px">
+        <p  >Pharma Franchise</p>
+        <p >drug Franchise</p>
+        <p >Pharma Franchise</p>
+       
+        </Card>
+      </Card>
+     </Box>  <Box textAlign="left" display="flex" height='160px' width="300px" boxShadow=" rgba(99, 99, 99, 0.2) 0px 2px 8px 0px" border="0 solid red">
+      <Card marginTop="28px" marginLeft="15px"><img height="110px" src="https://3.imimg.com/data3/BA/YT/MY-2018319/multidisciplinary-care-125x125.jpg" alt="" /></Card>
+      <Card marginLeft="15px" boxShadow="  0px">
+      <h5 >Wires and Cables</h5>
+        <Card lineHeight="20px" border="0px solid red"fontSize="14px" boxShadow="  0px">
+        <p  >House Cables</p>
+        <p >Amoured Wires</p>
+        <p >Power Cables </p>
+       
+        </Card>
+      </Card>
+     </Box>
+  <Box textAlign="left" display="flex" height='160px' width="300px" boxShadow=" rgba(99, 99, 99, 0.2) 0px 2px 8px 0px" border="0 solid red">
+      <Card marginTop="28px" marginLeft="15px"><img height="110px" src="https://5.imimg.com/data5/VB/IL/GLADMIN-2/street-light-125x125.jpg" alt="" /></Card>
+      <Card marginLeft="15px" boxShadow="  0px">
+      <h5 >Wires and Cables</h5>
+        <Card lineHeight="20px" border="0px solid red"fontSize="14px" boxShadow="  0px">
+        <p  > Pharma bottels</p>
+        <p >Invorter bottels</p>
+        <p >Electric  bottels </p>
+        </Card>
+      </Card>
+     </Box>
+     <Box textAlign="left" display="flex" height='160px' width="300px" boxShadow=" rgba(99, 99, 99, 0.2) 0px 2px 8px 0px" border="0 solid red">
+      <Card marginTop="28px" marginLeft="15px"><img height="110px" src="https://3.imimg.com/data3/BA/YT/MY-2018319/multidisciplinary-care-125x125.jpg" alt="" /></Card>
+      <Card marginLeft="15px" boxShadow="  0px">
+      <h5 >Sryrups bottels</h5>
+        <Card lineHeight="20px" border="0px solid red"fontSize="14px" boxShadow="  0px"> 
+        <p >Flood Lights</p>
+        <p >Street Lights</p>
+        <p >Panel Lights </p>
+       
+        </Card>
+      </Card>
+     </Box>
+</SimpleGrid>
+</Flex>
+</Card>
+
+
+
+
+</div>
+
+};
